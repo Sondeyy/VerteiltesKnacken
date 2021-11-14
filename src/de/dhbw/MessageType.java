8258b1d3e4654794752ea77531f1355db89bf5a3
n1klasD <67094918+n1klasD@users.noreply.github.com>
2021-11-14 20:02:40 +0100

    cleaned up MessageType

--- src/de/dhbw/MessageType.java
+++ src/de/dhbw/MessageType.java
@@ -1,16 +1,16 @@
 package de.dhbw;
 
 public enum MessageType {
-    OK,             // it's okay to calculate this section
-    NOK,            // it's not okay to calculate this section, I'm doing it!
-    FREE,           // is this section available?
-    RSA,            // start calculation! here is the public key. Payload: [pub]
-    START,          // Payload [pub, clientIP, clientPort]
-    FINISHED,       // I finished the calculation of my package
-    ALTERNATIVES,   // Here you have alternative worker addresses to connect to
-    ANSWER_FOUND,    // I did it, here is the solution!!!
-    JOIN, // initially join cluster to, get all cluster nodes to connect to
-    CONNECT_CLUSTER,
-    CLUSTER_INFO,
-    HELLO
+    OK,                 // it's okay to calculate this section
+    NOK,                // it's not okay to calculate this section, I'm doing it!
+    RSA,                // start calculation! here is the public key. Payload: [pub]
+    JOIN,               // I want all nodes in the cluster, so that I can connect to them
+    FREE,               // Is this section available?
+    HELLO,              // Simple test message
+    START,              // Payload [pub, clientIP, clientPort(Listener)]
+    FINISHED,           // I finished the calculation of my package
+    ALTERNATIVES,       // Here you have alternative worker addresses to connect to (if i ) --> obsolete, workers should connect
+    ANSWER_FOUND,       // I did it, here is the solution!!!
+    CLUSTER_INFO,       // Here are all members of the cluster for you to connect to
+    CONNECT_CLUSTER,    // Hey, I want to join the cluster, Payload [myAdress, ListenerPort]
 }
